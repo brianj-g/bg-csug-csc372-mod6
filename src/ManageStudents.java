4bc3a89bcfbc966c224ce70e5d5e9155e43d9543
Brian Gunther <brian.gunther@outlook.com>
2024-09-20 19:32:36 -0700

    files created and student class done

---
+++ src/ManageStudents.java
@@ -0,0 +1,9 @@
+
+public class ManageStudents {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Student.java
@@ -0,0 +1,55 @@
+
+public class Student {
+	private int rollno;
+	private String name;
+	private String address;
+	
+	/**
+	 * Getter for the roll number
+	 * @return the student's roll number
+	 */
+	public int getRollno() {
+		return rollno;
+	}
+	
+	/**
+	 * Setter for the roll number
+	 * @param rollno
+	 */
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	
+	/**
+	 * Getter for the name
+	 * @return the student's name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Setter for the name
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Getter for the address
+	 * @return the student's address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	
+	/**
+	 * Setter for the address
+	 * @param address
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
