3b97b46d9d11d67372395b71f5fc851715cb9183
Brian Gunther <brian.gunther@outlook.com>
2024-09-20 19:53:04 -0700

    Name and ID comparator classes done

--- src/IdComparator.java
+++ src/IdComparator.java
@@ -1,4 +1,12 @@
+import java.util.Comparator;
 
-public class IdComparator {
-
+/**
+ * Compares two integers using the Comparator interface
+ */
+public class IdComparator implements Comparator<Integer> {
+	@Override
+	public int compare(Integer first, Integer second) {
+		// Compare the two integers and return -1, 0, or 1 to declare their precedence
+		return first.compareTo(second);
+	}
 }
--- src/NameComparator.java
+++ src/NameComparator.java
@@ -1,4 +1,12 @@
+import java.util.Comparator;
 
-public class NameComparator {
-
+/**
+ * Compares two strings using the Comparator interface
+ */
+public class NameComparator implements Comparator<String>{
+	@Override
+	public int compare(String first, String second) {
+		// Compare the two strings and return -1, 0, or 1 to declare their precedence
+		return first.compareTo(second);
+	}
 }
