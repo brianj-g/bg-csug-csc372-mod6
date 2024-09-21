dbe0fd9147abe4d92c48b50a43dc08c090743257
Brian Gunther <brian.gunther@outlook.com>
2024-09-20 20:11:11 -0700

    committed

--- src/Sort.java
+++ src/Sort.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
 
+/**
+ * Use a selection sort algorithm to sort objects using Comparator classes
+ */
 public class Sort {
-
+	public static ArrayList<Student> sortName(ArrayList<Student> student, NameComparator nameCompare){
+		
+		
+		return student;
+	}
 }
